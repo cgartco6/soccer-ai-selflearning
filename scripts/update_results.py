8007139252e222b2788a3a034f46970747eb58ff
cgartco6 <155203611+cgartco6@users.noreply.github.com>
2026-05-02 15:30:49 +0200

    Create update_results.py

---
+++ scripts/update_results.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from src.pipeline import Pipeline
+
+if __name__ == "__main__":
+    pipe = Pipeline()
+    pipe.update_results()
+    print("Results updated.")
